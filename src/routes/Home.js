3ee80c1a6db88f1ffdcdd4dc7d1b0f9e33325239
jipark96 <wlgus996@naver.com>
2023-04-25 17:48:38 +0900

    delete, edit

---
+++ src/routes/Home.js
@@ -0,0 +1,58 @@
+import Nweet from "components/Nweet";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (e) => {
+    setNweet(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="지금 너의 상태는?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => {
+          return (
+            <Nweet
+              key={nweet.id}
+              nweetObj={nweet}
+              isOwner={nweet.creatorId === userObj.uid}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
